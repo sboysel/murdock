3d179a68f23092bbe2b52da01e26b2bb8526c04f
Sam Boysel <sboysel@gmail.com>
2016-07-18 19:20:46 -0400

    cleaning 'CodeType' factor levels

--- data-raw/murdock.R
+++ data-raw/murdock.R
@@ -9,14 +9,19 @@ murdock <- rgdal::readOGR(dsn = "data-raw/Murdock_EA_2011_vkZ.geojson",
 murdock <- murdock[!is.na(murdock$NAME),]
 murdock <- sp::spChFIDs(murdock, as.character(murdock$NAME))
 
+# Clean CodeType
+murdock$CodeType <- factor(ifelse(grepl("EA", murdock$CodeType), "In EA", "Not in EA"),
+                           levels = c("In EA", "Not in EA"),
+                           labels = c("In EA", "Not in EA"))
+
 ## TODO: relabel variables
 
 ## Save cleaned GeoJSON
-rgdal::writeOGR(obj = murdock,
-                dsn = "data-raw/Murdock_EA_2011_vkZ.geojson",
-                layer = "OGRGeoJSON",
-                overwrite_layer = TRUE,
-                driver = "GeoJSON")
+# rgdal::writeOGR(obj = murdock,
+#                 dsn = "data-raw/murdock1967_cleaned.geojson",
+#                 layer = "OGRGeoJSON",
+#                 overwrite_layer = TRUE,
+#                 driver = "GeoJSON")
 
 ## Check
 # murdock <- rgdal::readOGR(dsn = "data-raw/Murdock_EA_2011_vkZ.geojson",
